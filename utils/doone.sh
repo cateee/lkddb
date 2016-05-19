ab3faf91848f174e98f93e40e89d7bbcac19e5de
Giacomo Catenazzi <cate@cateee.net>
2016-05-19 14:44:27 +0000

    add utils/doone.sh
    TO do only one kernel version (with checkout)

---
+++ utils/doone.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#: utils/doall.sh : redo "all" released kernel
+#
+#  Copyright (c) 2007-2011  Giacomo A. Catenazzi <cate@cateee.net>
+#  This is free software, see GNU General Public License v2 (or later) for details
+#  or distributable with any GNU Documentation Public License
+
+
+set -e
+
+kdir="$HOME/kernel/"
+
+build_lkddb() {
+    time python ./build-lkddb.py -v -b lkddb -l lkddb-%.log -k "$1"
+    time python ./build-lkddb.py -v -b lkddb -l lkddb-%.log -k ~/kernel/linux/
+}
+
+
+do_git_kernel() {
+    echo "------ doing $1 --------"
+    ( cd "$kdir/linux"
+      git checkout "$1"
+      git clean -d -f -f
+      [ -d include/config/ ] || mkdir include/config/
+      [ -f include/config/auto.conf ] || echo "CONFIG_LOCALVERSION_AUTO=y" > include/config/auto.conf
+    )
+    build_lkddb "$kdir/linux/"
+}
+
+do_tar_kernel() {
+    echo "------ doing $1 --------"
+    ( cd "$kdir"
+      [ -d "linux-$1" ] || tar xjf "linux-$1.tar.bz2"
+    )
+    echo "$kdir/linux-$1"
+    build_lkddb "$kdir/linux-$1"
+}
+
+
+if [[ "$1" =~ ^v ]] ; then
+    do_git_kernel "$1"
+else
+    do_tar_kernel "$1"
+fi
+
+
+# rest to master
+( cd "$kdir/linux"
+    git checkout master
+    git clean -d -f -f
+    [ -d include/config/ ] || mkdir include/config/
+    [ -f include/config/auto.conf ] || echo "CONFIG_LOCALVERSION_AUTO=y" > include/config/auto.conf
+)
+
